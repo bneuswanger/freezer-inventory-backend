041477d68cad89db8e5a6f59eb3b1d7ba8fbbaab
Bryan Neuswanger <bneuswanger@gmail.com>
2022-11-08 17:54:09 -0600

    serve index.html for all /*

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/test', (req, res) => { res.send('Hello! Express server is running!')})
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html')))
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please set to production') )
 }
