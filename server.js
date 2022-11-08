859c5d2776967eb6eb62569c5bb66692e1438ad2
Bryan Neuswanger <bneuswanger@gmail.com>
2022-11-08 16:07:03 -0600

    configured DB connect before listening

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
-connectDB()
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
@@ -18,12 +20,18 @@ app.use('/api/users', require('./routes/userRoutes'))
 app.get('/test', (req, res) => { res.send('Hello! Express server is running!')})
 
 //Serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-// } else {
-//     app.get('/', (req, res) => res.send('Please set to production') )
-// }
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production') )
+}
+
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    })
+})
 
 app.use(errorHandler)
